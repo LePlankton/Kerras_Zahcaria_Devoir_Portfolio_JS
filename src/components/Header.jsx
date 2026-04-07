0351fc6b7e58446fdb0088fa9c1eca393fdf932a
Kerras Zahcaria <krsv2v.officiel@gmail.com>
2026-04-07 11:21:37 +0200

    init: mise en place du projet React avec structure de base

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {         // On crée une fonction qui s'appelle Header, c'est un composant React qui va retourner du JSX
+  return (                  // Cette fonction retourne du JSX (HTML-like syntax) qui sera rendu dans le DOM
+    <div>Header</div>       // Le contenu affiché à l'écran
+  )
+}
+
+export default Header       // On exporte le composant Header pour pouvoir l'utiliser dans d'autres fichiers de notre application React
---
+++ src/pages/MentionsLegales.jsx
@@ -0,0 +1,7 @@
+function MentionsLegales() {
+  return (
+    <div>Mentions Légales</div>
+  )
+}
+
+export default MentionsLegales
