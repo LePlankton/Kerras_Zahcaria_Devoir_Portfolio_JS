0eb8e4f24eb68e044bc8fd135c24a5e6f14af721
Kerras Zahcaria <krsv2v.officiel@gmail.com>
2026-04-07 18:46:18 +0200

    feat: ajout de la page Portfolio avec 6 cards

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,111 @@
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <main>
+      <section style={{
+        backgroundImage: "url('/images/banner.jpg')",
+        height: "200px",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}></section>
+
+      <section className="container text-center my-5">
+        <h2 className="fw-bold">Portfolio</h2>
+        <p>Voici quelques-unes de mes réalisations.</p>
+        <hr className="border-primary border-3 opacity-100 w-50 mx-auto"/>
+      </section>
+
+      <section className="container mb-5">
+        <div className="row">
+          
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/fresh-food.jpg" alt="Fresh Food" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">Fresh Food</h5>
+                <p className="card-text">Site de vente de produits frais en ligne</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  Site réalisé avec PHP et MySQL
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/restaurant-japonais.jpg" alt="Restaurant Akira" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">Restaurant Akira</h5>
+                <p className="card-text">Site de vente de produits frais en ligne</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  Site réalisé avec Wordpress
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/espace-bien-etre.jpg" alt="Espace bien-être" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">Espace bien-être</h5>
+                <p className="card-text">Site de vente de produits en ligne</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  Site réalisé avec LARAVEL
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/seo.jpg" alt="SEO" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">SEO</h5>
+                <p className="card-text">Amélioration du référencement d'un site</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  Utilisation des outils SEO
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/coder.jpg" alt="Création d'une API" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">Création d'une API</h5>
+                <p className="card-text">Création d'une API RESTFULL publique</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  PHP - SYMFONY
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center p-0">
+                <img src="/images/portfolio/screens.jpg" alt="Maquette d'un site web" className="img-fluid mb-3"/>
+                <h5 className="card-title fw-bold">Maquette d'un site web</h5>
+                <p className="card-text">Création du prototype d'un site</p>
+                <a href="#" className="btn btn-primary mt-3">Voir le site</a>
+                <div className="card-footer text-muted text-center mt-3">
+                  Réalisée avec FIGMA
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
+      </section>
+
+    </main>
   )
 }
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -18,45 +18,44 @@ function Services() {
         <div className="row">
           
           <div className="col-md-4 mb-4">
-          <div className="card h-100">
-            <div className="card-body text-center">
-              <i className="bi bi-brush fs-1 text-primary mb-3 d-block"></i>
-              <h5 className="card-title fw-bold">UX Design</h5>
-              <p className="card-text">L'UX Design est une discipline qui consiste à concevoir des produits
-                (site web, applications mobiles, logiciels, objets connectés, etc...) en plaçant 
-                l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur
-                la plus fluide et agréable possible.
-              </p>
+            <div className="card h-100">
+              <div className="card-body text-center">
+                <i className="bi bi-brush fs-1 text-primary mb-3 d-block"></i>
+                <h5 className="card-title fw-bold">UX Design</h5>
+                <p className="card-text">L'UX Design est une discipline qui consiste à concevoir des produits
+                  (site web, applications mobiles, logiciels, objets connectés, etc...) en plaçant 
+                  l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur
+                  la plus fluide et agréable possible.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-md-4 mb-4">
-          <div className="card h-100">
-            <div className="card-body text-center">
-              <i className="bi bi-code-slash fs-1 text-primary mb-3 d-block"></i>
-              <h5 className="card-title fw-bold">Développement web</h5>
-              <p className="card-text">Le développement de sites web consiste à créer des sites internet en
-                utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc...) et des frameworks
-               (Bootstrap, React, Angular, etc...).
-              </p>
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center">
+                <i className="bi bi-code-slash fs-1 text-primary mb-3 d-block"></i>
+                <h5 className="card-title fw-bold">Développement web</h5>
+                <p className="card-text">Le développement de sites web consiste à créer des sites internet en
+                  utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc...) et des frameworks
+                (Bootstrap, React, Angular, etc...).
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-md-4 mb-4">
-          <div className="card h-100">
-            <div className="card-body text-center">
-              <i className="bi bi-search fs-1 text-primary mb-3 d-block"></i>
-              <h5 className="card-title fw-bold">Référencement</h5>
-              <p className="card-text">Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web
-                pour le faire remonter dans les résultas des moteurs de recherche (Google, Bing, Yahoo, etc...). L'objectif 
-                est d'attirer un maximum de visiteurs qualifiés sur le site.
-              </p>
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body text-center">
+                <i className="bi bi-search fs-1 text-primary mb-3 d-block"></i>
+                <h5 className="card-title fw-bold">Référencement</h5>
+                <p className="card-text">Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web
+                  pour le faire remonter dans les résultas des moteurs de recherche (Google, Bing, Yahoo, etc...). L'objectif 
+                  est d'attirer un maximum de visiteurs qualifiés sur le site.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-
         </div>
       </section>
     </main>
